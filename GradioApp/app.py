9f76bad4a1ab43cf85e9d21da99605560c7a531e
Nuvantim <71752646+Nuvantim@users.noreply.github.com>
2025-11-24 11:42:04 +0700

    Update app.py

--- GradioApp/app.py
+++ GradioApp/app.py
@@ -6,8 +6,6 @@ import joblib
 import re
 import gradio as gr
 
-# Load model dan komponen
-
 onnx_model_path = "models/maxim-sentiment-models.onnx"
 vec_model_path  = "models/maxim_fasttext.vec"
 label_encoder_path = "models/label_encoder.pkl"
@@ -24,32 +22,25 @@ session = ort.InferenceSession(onnx_model_path)
 input_name = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 
-# Fungsi konversi teks ke vektor
-
 def review_to_vec(tokens, model):
     vecs = [model[word] for word in tokens if word in model]
     return np.mean(vecs, axis=0) if len(vecs) > 0 else np.zeros(model.vector_size)
 
-# Fungsi prediksi sentimen
-
 def predict_sentiment(text):
-    # Bersihkan teks
     tokens = re.sub(r'[^a-zA-Z0-9\s]', ' ', emoji.replace_emoji(text.lower(), '')).split()
-    
-    # Konversi ke vektor
     vec = review_to_vec(tokens, ft_model)
     input_tensor = vec.reshape(1, 1, -1).astype(np.float32)
     
-    # Jalankan model ONNX
     outputs = session.run([output_name], {input_name: input_tensor})
     pred_class = np.argmax(outputs[0], axis=1)[0]
-    
-    # Decode label hasil prediksi
-    pred_label = le.inverse_transform([pred_class])[0]
-    return f"🟢 Sentimen: {pred_label}"
+    pred_label = le.inverse_transform([pred_class])[0].upper()
 
-
-# Gradio Interface
+    if pred_label == "POSITIF":
+        return "✅ Sentimen: POSITIF"
+    elif pred_label == "NEGATIF":
+        return "⛔ Sentimen: NEGATIF"
+    else:
+        return f"ℹ️ Sentimen: {pred_label}"
 
 interface = gr.Interface(
     fn=predict_sentiment,
@@ -62,4 +53,3 @@ interface = gr.Interface(
 
 if __name__ == "__main__":
     interface.launch(server_name="127.0.0.1", server_port=7860)
-
